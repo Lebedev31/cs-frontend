4cc5425d3fe1d39a698cf0c601ac8f0d4b00e579
Lebed <tireonreg@gmail.com>
2025-11-16 23:52:35 +0300

    3

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,19 @@
-import { Metadata } from "next";
+import { Suspense } from "react";
 import RegisterBlock from "@/Components/Registration/RegisterBlock";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Вход в аккаунт - Авторизация на CS Rating",
   description:
     "Войдите в личный кабинет CS Rating для управления серверами, просмотра статистики и использования дополнительных возможностей.",
-  robots: { index: false, follow: true }, // Не индексируем страницу входа
+  robots: { index: false, follow: true },
   alternates: { canonical: "/login" },
 };
 
 export default function Login() {
-  return <RegisterBlock />;
+  return (
+    <Suspense fallback={<div>Проверка параметров...</div>}>
+      <RegisterBlock />
+    </Suspense>
+  );
 }
